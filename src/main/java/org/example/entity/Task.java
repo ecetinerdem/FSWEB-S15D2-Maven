7d384cd1b8753f4f23edf27284ae58676b3752d0
Cetin Erdem <cetoerdem1@gmail.com>
2024-11-27 16:12:56 +0400

    finished tasks

--- src/main/java/org/example/entity/Task.java
+++ src/main/java/org/example/entity/Task.java
@@ -6,15 +6,16 @@ public class Task {
     private String project;
     private String description;
     private String assignee;
-    private Priority priority;
     private Status status;
+    private Priority priority;
 
-    public Task(String project, String description, String assignee, Priority priority, Status status) {
+
+    public Task(String project, String description, String assignee, Status status, Priority priority) {
         this.project = project;
         this.description = description;
         this.assignee = assignee;
-        this.priority = priority;
         this.status = status;
+        this.priority = priority;
     }
 
     public String getProject() {
@@ -61,11 +62,22 @@ public class Task {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Task task)) return false;
-        return Objects.equals(project, task.project) && Objects.equals(description, task.description) && Objects.equals(assignee, task.assignee) && priority == task.priority && status == task.status;
+        return Objects.equals(project, task.project) && Objects.equals(description, task.description);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(project, description, assignee, priority, status);
+        return Objects.hash(project, description);
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "project='" + project + '\'' +
+                ", description='" + description + '\'' +
+                ", assignee='" + assignee + '\'' +
+                ", status=" + status +
+                ", priority=" + priority +
+                '}';
     }
 }
--- src/main/java/org/example/entity/TaskData.java
+++ src/main/java/org/example/entity/TaskData.java
@@ -1,5 +1,7 @@
 package org.example.entity;
 
+import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class TaskData {
@@ -14,4 +16,56 @@ public class TaskData {
         this.bobsTasks = bobsTasks;
         this.unassignedTasks = unassignedTasks;
     }
+
+    public Set<Task> getTasks(String name) {
+        switch (name) {
+            case "ann":
+                return this.annsTasks;
+            case "bob":
+                return this.bobsTasks;
+            case "carol":
+                return this.carolsTasks;
+            case "all":
+                return getUnion(annsTasks, bobsTasks, carolsTasks);
+            default:
+                return new HashSet<>();
+        }
+    }
+
+    public Set<Task> getUnion(Set<Task>... sets) {
+        HashSet<Task> allTasks = new LinkedHashSet<>();
+        for (Set<Task> tasks : sets) {
+            allTasks.addAll(tasks); // Use addAll to add all elements from the set.
+        }
+        return allTasks;
+    }
+
+    public Set<Task> getIntersection(Set<Task> first, Set<Task> second) {
+        Set<Task> intersection = new HashSet<>(first);
+        intersection.retainAll(second);
+        return intersection;
+    }
+
+    public Set<Task> getDifferences(Set<Task> first, Set<Task> second) {
+        Set<Task> difference = new HashSet<>(first);
+        difference.removeAll(second);
+        return difference;
+    }
+
+    public Set<Task> getAnnsTasks() {
+        return annsTasks;
+    }
+
+    public Set<Task> getBobsTasks() {
+        return bobsTasks;
+    }
+
+    public Set<Task> getCarolsTasks() {
+        return carolsTasks;
+    }
+
+    public Set<Task> getUnassignedTasks() {
+        return unassignedTasks;
+    }
+
 }
